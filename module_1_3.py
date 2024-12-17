e57aeb04b17a297b2dd803cb3f502d6207e88acc
MoskvinAlexandr75 <alexandermoskvin2@gmail.com>
2024-12-17 19:58:50 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name = "Aleksander"
+print (name ,type ("neme"))
+print("ade"+"49")
+age=49
+print(age, type(age))
+new_age=age+10
+print(new_age)
+Aleksander="is_student"
+print(Aleksander=="is_student")
+
+
+
+
